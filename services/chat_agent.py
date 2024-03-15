e9829bb0fd9a2207ca0d7ced321e212f42943c93
Amirthanathan R <amirth300324@gmail.com>
2024-03-15 23:10:37 +0530

    Added Chat agent

--- services/chat_agent.py
+++ services/chat_agent.py
@@ -1,11 +1,12 @@
 """A module for class `ChatAgent`"""
-from typing import List
 from langchain.vectorstores.chroma import Chroma
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import SystemMessage, BaseMessage
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from langchain_community.llms.ollama import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import (SystemMessagePromptTemplate,
+                                    HumanMessagePromptTemplate,
+                                    PromptTemplate)
 
 SYSTEM_MESSAGE = """You're a AI bot built to answer questions about \
 a university. Converse with the user for the provided context
@@ -16,41 +17,74 @@ Context: {context}
 
 class ChatAgent():
     def __init__(self, vec_db: Chroma, init_sys_msg: str = SYSTEM_MESSAGE):
-        self.prompt = ChatPromptTemplate.from_messages([
-            SystemMessage(content=init_sys_msg),
-            MessagesPlaceholder(variable_name="messages")
-        ])
+        self.client = ChatGoogleGenerativeAI(
+                model="gemini-pro",
+                convert_system_message_to_human=True,
+                stream=True)
+        sys_prompt = SystemMessagePromptTemplate(
+                prompt=PromptTemplate(
+                    input_variables=["context"],
+                    template=init_sys_msg
+                    )
+                )
+        human_prompt = HumanMessagePromptTemplate(
+                prompt=PromptTemplate(
+                    input_variables=["question"],
+                    template="{question}"
+                    )
+                )
+        self.prompt = ChatPromptTemplate(
+                input_variables=["context", "question"],
+                messages=[
+                    sys_prompt, human_prompt
+                    ]
+                )
 
-        self.client = Ollama(model="tinyllama")
         self.chain = (
                 RunnableParallel({
-                    "context": vec_db.as_retriever(search_type="mmr"),
-                    "messages": RunnablePassthrough()
-                    })
+                        "context": vec_db.as_retriever(search_type="mmr"),
+                        "question": RunnablePassthrough()
+                })
                 | self.prompt
                 | self.client
                 | StrOutputParser()
                 )
 
-    async def talk(self, messages: List[BaseMessage]):
+    def talk(self, question):
         """Returns the async stream of llm's output."""
-        await self.chain.ainvoke(messages)
+        return self.chain.stream(question)
 
 
 if __name__ == "__main__":
+    from functools import reduce
     from langchain.memory import ChatMessageHistory
-    import asyncio
+    from langchain.document_loaders import TextLoader
+    from langchain_text_splitters import CharacterTextSplitter
+    from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-    async def main(db, chatbot, mess_his):
+    def main(db, chatbot, mess_his):
         while True:
             mess = input(">>>")
-            mess_his.add_user_message(mess)
-            ai_mess = await chatbot.talk(mess_his.messages)
-            mess_his.add_ai_message(ai_mess)
-            print(ai_mess)
+            ai_mess = chatbot.talk(mess)
+            for chr in ai_mess:
+                print(chr, end='')
+            print()
 
-    db = Chroma()
+    print("Starting Client")
+    print("Loading Database")
+    files = ['B.E_CS_(AI&ML)Handbook_R2021.txt']
+
+    files = list(map(lambda x: TextLoader('documents/'+x), files))
+    splitter = CharacterTextSplitter('\n', chunk_size=1000, chunk_overlap=0)
+    docs = list(map(lambda x: splitter.split_documents(x.load()), files))
+    docs = reduce(lambda x, y: x+y, docs)
+    db = Chroma(
+            persist_directory="chroma_data/",
+            embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
+            )
+
+    print("Loaded Database")
     chatbot = ChatAgent(db)
     mess_his = ChatMessageHistory()
 
-    asyncio.run(main(db, chatbot, mess_his))
+    main(db, chatbot, mess_his)
